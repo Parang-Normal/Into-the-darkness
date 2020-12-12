540c8197437dc09fd5381395a0671f910160d65a
Roi <Rowee723@users.noreply.github.com>
2020-12-12 23:55:37 +0800

    Added scenes and spawner scripts
    Added new level scenes and added enemy spawn script

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public delegate void EnemyKilled();
+
+    public static event EnemyKilled OnEnemyKilled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void die()
+    {
+        if (OnEnemyKilled != null)
+        {
+            OnEnemyKilled();
+        }
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Transform[] EnemySpawnPoint;
+    public GameObject EnemyPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemy();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Enable()
+    {
+        EnemyController.OnEnemyKilled += SpawnEnemy;
+    }
+
+    void SpawnEnemy()
+    {
+        int randomPoint = Mathf.RoundToInt(Random.Range(0f, EnemySpawnPoint.Length - 1));
+        Instantiate(EnemyPrefab, EnemySpawnPoint[randomPoint].transform.position, Quaternion.identity);
+    }
+}
