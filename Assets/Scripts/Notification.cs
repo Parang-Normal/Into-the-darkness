ad4945879e5ecf15460bd43b6cc63b98d1241686
Roi <Rowee723@users.noreply.github.com>
2020-12-19 01:52:35 +0800

    Added notifications

---
+++ Assets/Scripts/Notification.cs
@@ -0,0 +1,51 @@
+﻿
+using  System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Notifications.Android;
+
+public class Notification : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        BuiltDefaultNotifChannel();
+    }
+
+    public void SendSimpleNotif()
+    {
+        string title = "Simple Notif";
+        string text = "This is a simple notif";
+        DateTime fireTime = DateTime.Now.AddSeconds(10);
+
+        var notif = new AndroidNotification(title,text,fireTime);
+        AndroidNotificationCenter.SendNotification(notif, "default");
+
+    }
+    public void BuiltDefaultNotifChannel()
+    {
+        string channel_id = "default";
+
+        string channel_name = "Default Channel";
+
+        Importance importance = Importance.Default;
+
+        string channel_description = "Default channel for this game";
+
+       var channel = new AndroidNotificationChannel(channel_id, channel_name, channel_description,importance);
+
+       AndroidNotificationCenter.RegisterNotificationChannel(channel);
+    }
+}
